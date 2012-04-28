17f39b4dc8b82970b2fe4121ca5ea52f69beed9b
fil <fil@madagaskar.(none)>
2012-04-28 12:13:56 +0300

    added static files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
-};
+module.exports = {
+  index: function(req, res){
+    res.render('index', { title: 'Express' })
+  }
+}
