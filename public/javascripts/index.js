3e6f00d0bbb773963dfb65377b59ce703de482ac
wice <wice24@gmail.com>
2012-04-28 10:16:56 -0400

    add timeline

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,39 @@
 var onLinkedInLoad = function () {
   IN.Event.on(IN, 'auth', function () {
     IN.API.Profile('me').result(function () {
-      console.log(arguments);
+			var TimeLine = (function(){
+				var timeLine = function(arrayOfDates){
+					var self = this;
+					self.createFile(arrayOfDates,function(data){
+						self.init();
+					});
+				}
+				timeLine.prototype = {
+					init: function(){
+						var self = this;
+						$(function(){
+							var timeline = new VMM.Timeline();
+							timeline.init('data1.json');
+						});
+					},
+					createFile: function(json, callback){
+						var self = this;
+						$.get('/ajax/create/?json=' + JSON.stringify(json), function(data){
+								callback(data);
+						});
+					}
+				} 
+				return new timeLine([{
+						startDate:'2001,4',
+						headline:'Developed an iPad newspaper applicationttittle',
+						text:'Developed an iPad newspaper applicationblabla'
+						},
+						{startDate:'2010,8',
+						headline:'Developed an iPad newspaper applicationttittle',
+						text:'Developed an iPad newspaper applicationblabla'
+						}]
+				);
+			})();
     })
   })
 }
